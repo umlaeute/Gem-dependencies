a9c8a94e2c2490e5610e605f63d991b59342004a
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2017-04-25 20:24:39 +0200

    brew's glfw packages have been renamed

--- brew-GemDeps.sh
+++ brew-GemDeps.sh
@@ -10,4 +10,4 @@ install_upgrade pkg-config gettext
 brew link gettext --force
 install_upgrade fribidi --universal
 install_upgrade imagemagick ftgl
-install_upgrade sdl homebrew/versions/glfw2 homebrew/versions/glfw3
+install_upgrade sdl glfw@2 glfw
