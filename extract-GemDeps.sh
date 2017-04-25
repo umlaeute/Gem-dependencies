cd8efbd7d6aea16684b58827c652ce063227d1c3
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2017-04-25 17:08:32 +0200

    script to extract GemDeps (1st draft)

---
+++ extract-GemDeps.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+## extract Gem dependencies to their proper places
+# platform: OSX (only!)
+# requires: root-priviliges
+
+
+error() {
+    echo "$@" 1>&2
+}
+
+usage() {
+    error "usage: $0 [ <depsdir>]"
+    error "This script is intended to be included into  GemDeps release packages,"
+    error " as distributed via https://github.com/umlaeute/Gem-dependencies/releases/"
+    error "It can then be run to extract the contents of such a package"
+    error " to the proper places..."
+    error
+    exit 1
+}
+checkpkg() {
+if [  ! -e "${BASEDIR}/$1" ]; then
+    error "ERROR: missing package '$1'"
+    error
+    usage
+fi
+}
+
+extract_brew() {
+    checkpkg brew.tgz
+    tar -xvf brew.tgz --directory /usr/local/Cellar
+    tar -tf  brew.tgz | grep "/$" | grep -v "/.*/" | while read p; do
+        pkg=${p%/}
+        brew link ${pkg}
+    done                                                   
+}
+extract_deps() {
+    checkpkg deps.tgz
+    tar -xvf deps.tgz --strip-components --directory "${TARGETDIR}"
+}
+
+
+BASEDIR=${0%/*}
+TARGETDIR=$1
+
+checkpkg brew.tgz
+checkpkg deps.tgz
+if [ -z "${TARGETDIR}" ]; then
+    TARGETDIR=$(pwd)
+fi
+mkdir -p "${TARGETDIR}"
+
+extract_brew
+extract_deps
